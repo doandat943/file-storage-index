a9a621d547dd561594e780dfd245c5778dcd7ee2
doandat943 <doandat943@joverse.us>
2025-09-11 18:40:44 +0700

    refactor(fontawesome): streamline icon imports and library setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,133 +1,46 @@
-import '@fortawesome/fontawesome-svg-core/styles.css'
-
-import '../styles/globals.css'
-import '../styles/markdown-github.css'
-import { Analytics } from '@vercel/analytics/react';
-import { CookiesProvider } from 'react-cookie'
-
-// Require had to be used to prevent SSR failure in Next.js
-// Related discussion: https://github.com/FortAwesome/Font-Awesome/issues/19348
-const { library, config } = require('@fortawesome/fontawesome-svg-core')
-config.autoAddCss = false
-
-import {
-  faFileImage,
-  faFilePdf,
-  faFileWord,
-  faFilePowerpoint,
-  faFileExcel,
-  faFileAudio,
-  faFileVideo,
-  faFileArchive,
-  faFileCode,
-  faFileAlt,
-  faFile,
-  faFolder,
-  faCopy,
-  faArrowAltCircleDown,
-  faTrashAlt,
-  faEnvelope,
-  faFlag,
-  faCheckCircle,
-} from '@fortawesome/free-regular-svg-icons'
-import {
-  faSearch,
-  faPen,
-  faCheck,
-  faPlus,
-  faMinus,
-  faCopy as faCopySolid,
-  faAngleRight,
-  faDownload,
-  faMusic,
-  faArrowLeft,
-  faArrowRight,
-  faFileDownload,
-  faUndo,
-  faBook,
-  faKey,
-  faSignOutAlt,
-  faCloud,
-  faChevronCircleDown,
-  faChevronDown,
-  faLink,
-  faExternalLinkAlt,
-  faExclamationCircle,
-  faExclamationTriangle,
-  faTh,
-  faThLarge,
-  faThList,
-  faHome,
-  faLanguage,
-  faCircleNotch,
-  faSun,
-  faMoon,
-  faDesktop,
-} from '@fortawesome/free-solid-svg-icons'
-import * as Icons from '@fortawesome/free-brands-svg-icons'
 
 import type { AppProps } from 'next/app'
 import NextNProgress from 'nextjs-progressbar'
 import { appWithTranslation } from 'next-i18next'
+import { Analytics } from '@vercel/analytics/react';
+import { CookiesProvider } from 'react-cookie'
 
-// import all brand icons with tree-shaking so all icons can be referenced in the app
-const iconList = Object.keys(Icons)
-  .filter(k => k !== 'fab' && k !== 'prefix')
-  .map(icon => Icons[icon])
+import '../styles/globals.css'
+import '../styles/markdown-github.css'
+import '@fortawesome/fontawesome-svg-core/styles.css'
 
-library.add(
-  faFileImage,
-  faFilePdf,
-  faFileWord,
-  faFilePowerpoint,
-  faFileExcel,
-  faFileAudio,
-  faFileVideo,
-  faFileArchive,
-  faFileCode,
-  faFileAlt,
-  faFile,
-  faFlag,
-  faFolder,
-  faMusic,
-  faArrowLeft,
-  faArrowRight,
-  faAngleRight,
-  faFileDownload,
-  faCopy,
-  faCopySolid,
-  faPlus,
-  faMinus,
-  faDownload,
-  faLink,
-  faUndo,
-  faBook,
-  faArrowAltCircleDown,
-  faKey,
-  faTrashAlt,
-  faSignOutAlt,
-  faEnvelope,
-  faCloud,
-  faChevronCircleDown,
-  faExternalLinkAlt,
-  faExclamationCircle,
-  faExclamationTriangle,
-  faHome,
-  faCheck,
-  faCheckCircle,
-  faSearch,
-  faChevronDown,
-  faTh,
-  faThLarge,
-  faThList,
-  faLanguage,
-  faPen,
-  faCircleNotch,
-  faSun,
-  faMoon,
-  faDesktop,
-  ...iconList
-)
+// FontAwesome configuration for v3.x - using require() for compatibility
+import { config } from '@fortawesome/fontawesome-svg-core'
+config.autoAddCss = false
+
+// Use require for library to ensure proper SSR support
+const { library } = require('@fortawesome/fontawesome-svg-core')
+
+// Import all icon packages using require for stability
+const solidIcons = require('@fortawesome/free-solid-svg-icons')
+const regularIcons = require('@fortawesome/free-regular-svg-icons') 
+const brandIcons = require('@fortawesome/free-brands-svg-icons')
+
+// Add all icons from solid pack
+Object.keys(solidIcons).forEach(key => {
+  if (key !== 'fas' && key !== 'prefix' && solidIcons[key].iconName) {
+    library.add(solidIcons[key])
+  }
+})
+
+// Add all icons from regular pack
+Object.keys(regularIcons).forEach(key => {
+  if (key !== 'far' && key !== 'prefix' && regularIcons[key].iconName) {
+    library.add(regularIcons[key])
+  }
+})
+
+// Add all icons from brand pack
+Object.keys(brandIcons).forEach(key => {
+  if (key !== 'fab' && key !== 'prefix' && brandIcons[key].iconName) {
+    library.add(brandIcons[key])
+  }
+})
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
